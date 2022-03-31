136e7fbfde46d81a78de5fb675c499d26661b9e2
Yomguithereal <guillaumeplique@gmail.com>
2022-03-31 17:19:23 +0200

    halo renderer

--- renderers/example.ts
+++ renderers/example.ts
@@ -5,7 +5,8 @@ import { cropToLargestConnectedComponent } from "graphology-components";
 import randomLayout from "graphology-layout/random";
 import forceAtlas2 from "graphology-layout-forceatlas2";
 
-import NodeRichProgram from "./node/rich";
+import NodeThreeCircles from "./node/three-circles";
+import NodeHaloProgram from "./node/halo";
 
 const graph = clusters(UndirectedGraph, { clusters: 3, order: 100, size: 1000, clusterDensity: 0.8 });
 cropToLargestConnectedComponent(graph);
@@ -14,9 +15,13 @@ randomLayout.assign(graph);
 forceAtlas2.assign(graph, { iterations: 100, settings: forceAtlas2.inferSettings(graph) });
 
 graph.updateEachNodeAttributes((node, attr) => {
-  return { ...attr, size: 10, insideColor: "yellow", dotColor: "black" };
+  return { ...attr, size: 30, insideColor: "yellow", dotColor: "black" };
 });
 
 const container = document.getElementById("container") as HTMLDivElement;
 
-const renderer = new Sigma(graph, container, { nodeProgramClasses: { circle: NodeRichProgram } });
+const renderer = new Sigma(graph, container, {
+  nodeProgramClasses: { threeCircles: NodeThreeCircles, halo: NodeHaloProgram },
+  defaultNodeType: "halo",
+  defaultNodeColor: "lightblue",
+});
