137127608449848c7415b822d94120330356eaaf
Yomguithereal <guillaumeplique@gmail.com>
2022-11-16 17:54:44 +0100

    Despair about loops

--- renderers/src/edge/edge.loop.ts
+++ renderers/src/edge/edge.loop.ts
@@ -1,20 +1,27 @@
-import { NodeDisplayData, EdgeDisplayData } from "sigma/types";
-import { floatColor } from "sigma/utils";
+import type { NodeDisplayData, EdgeDisplayData } from "sigma/types";
+import type { RenderParams } from "sigma/rendering/webgl/programs/common/program";
 import { EdgeProgram } from "sigma/rendering/webgl/programs/common/edge";
-import { RenderParams } from "sigma/rendering/webgl/programs/common/program";
+import { floatColor } from "sigma/utils";
+
+interface EdgeDisplayDataWithLoopInformation extends EdgeDisplayData {
+  offset?: number;
+}
 
 const VERTEX_SHADER_SOURCE = /*glsl*/ `
 attribute vec2 a_position;
 attribute float a_size;
-attribute float a_nodeSize;
+attribute float a_thickness;
+attribute float a_angle;
 attribute vec4 a_color;
 
-uniform vec2 u_dimensions;
-uniform float u_sizeRatio;
-uniform float u_pixelRatio;
 uniform mat3 u_matrix;
+uniform float u_sizeRatio;
+uniform float u_correctionRatio;
+uniform vec2 u_dimensions;
 
 varying vec4 v_color;
+varying vec2 v_diffVector;
+varying float v_radius;
 varying float v_border;
 varying float v_borderRatio;
 
@@ -33,35 +40,33 @@ vec2 viewportToClipspace(vec2 pos, vec2 dimensions) {
 }
 
 const float bias = 255.0 / 254.0;
-const float theta = 0.78;
-const float borderRatio = 0.1;
+const float marginRatio = 1.05;
+const float theta = 0.0;
+const float minBorderRatio = 0.05;
 
 void main() {
-  vec2 position = (u_matrix * vec3(a_position, 1)).xy;
+  float size = a_size * u_correctionRatio / u_sizeRatio * 4.0;
+  vec2 diffVector = size * vec2(cos(a_angle), sin(a_angle));
+  vec2 position = a_position + diffVector  * marginRatio;
+
+  float viewportRadius = a_size * u_sizeRatio / (2.0 + marginRatio) - a_thickness * u_sizeRatio;
+
   vec2 viewportPosition = clipspaceToViewport(position, u_dimensions);
 
-  // Multiply the point size twice:
-  //  - x SCALING_RATIO to correct the canvas scaling
-  //  - x 2 to correct the formulae
-  float nodeRadius = a_nodeSize / u_sizeRatio * u_pixelRatio;
-  float loopRadius = a_size / u_sizeRatio * u_pixelRatio;
-
-  gl_PointSize = loopRadius * 2.0;
-
-  viewportPosition.x += loopRadius * cos(theta) * nodeRadius / nodeRadius;
-  viewportPosition.y += loopRadius * sin(theta);
-  // viewportPosition.y += (loopRadius/ 2.0 + nodeRadius);
+  viewportPosition.x -= viewportRadius;
+  viewportPosition.y += viewportRadius;
 
   gl_Position = vec4(
-    viewportToClipspace(viewportPosition, u_dimensions),
+    (u_matrix * vec3(viewportToClipspace(viewportPosition, u_dimensions), 1)).xy,
     0,
     1
   );
 
-  v_border = (0.5 / a_size) * u_sizeRatio;
-  v_borderRatio = 0.5 - borderRatio / 2.0;
+  v_border = u_correctionRatio;
+  v_borderRatio = 1.0 - max(minBorderRatio, a_thickness / a_size / u_sizeRatio * 1.5);
+  v_diffVector = diffVector;
+  v_radius = size / 2.0 / marginRatio;
 
-  // Extract the color:
   v_color = a_color;
   v_color.a *= bias;
 }
@@ -71,69 +76,113 @@ const FRAGMENT_SHADER_SOURCE = /*glsl*/ `
 precision mediump float;
 
 varying vec4 v_color;
+varying vec2 v_diffVector;
+varying float v_radius;
 varying float v_border;
 varying float v_borderRatio;
 
-const float radius = 0.5;
 const vec4 transparent = vec4(0.0, 0.0, 0.0, 0.0);
 
 void main(void) {
-  float distToCenter = length(gl_PointCoord - vec2(0.5, 0.5));
+  float borderRadius = v_borderRatio * v_radius;
+  float distToCenter = length(v_diffVector);
 
-  if (distToCenter < v_borderRatio - v_border)
+  // Out of disc
+  if (distToCenter < borderRadius - v_border) {
     gl_FragColor = transparent;
-  else if (distToCenter < v_borderRatio)
-    gl_FragColor = mix(v_color, transparent, (v_borderRatio - distToCenter) / v_border);
-  else if (distToCenter < radius - v_border)
+  }
+  // Antialiasing between outside and border
+  else if (distToCenter < borderRadius) {
+    gl_FragColor = mix(v_color, transparent, (borderRadius - distToCenter) / v_border);
+  }
+  // Border
+  else if (distToCenter < v_radius - v_border) {
     gl_FragColor = v_color;
-  else if (distToCenter < radius)
-    gl_FragColor = mix(transparent, v_color, (radius - distToCenter) / v_border);
-  else
+  }
+  // Antialiasing between border and transparent middle
+  else if (distToCenter < v_radius) {
+    gl_FragColor = mix(transparent, v_color, (v_radius - distToCenter) / v_border);
+  }
+  // Else
+  else {
     gl_FragColor = transparent;
+  }
 }
+
 `;
 
-const { UNSIGNED_BYTE, FLOAT } = WebGLRenderingContext;
+const ANGLE_1 = 0;
+const ANGLE_2 = (2 * Math.PI) / 3;
+const ANGLE_3 = (4 * Math.PI) / 3;
 
-const UNIFORMS = ["u_sizeRatio", "u_pixelRatio", "u_matrix", "u_dimensions"] as const;
+const UNIFORMS = ["u_sizeRatio", "u_correctionRatio", "u_matrix", "u_dimensions"] as const;
+
+const { FLOAT, UNSIGNED_BYTE } = WebGLRenderingContext;
 
 export default class EdgeLoopProgram extends EdgeProgram<typeof UNIFORMS[number]> {
   getDefinition() {
     return {
-      VERTICES: 1,
-      ARRAY_ITEMS_PER_VERTEX: 5,
+      VERTICES: 3,
+      ARRAY_ITEMS_PER_VERTEX: 6,
       VERTEX_SHADER_SOURCE,
       FRAGMENT_SHADER_SOURCE,
       UNIFORMS,
       ATTRIBUTES: [
         { name: "a_position", size: 2, type: FLOAT },
         { name: "a_size", size: 1, type: FLOAT },
-        { name: "a_nodeSize", size: 1, type: FLOAT },
+        { name: "a_thickness", size: 1, type: FLOAT },
         { name: "a_color", size: 4, type: UNSIGNED_BYTE, normalized: true },
+        { name: "a_angle", size: 1, type: FLOAT },
       ],
     };
   }
 
-  processVisibleItem(i: number, sourceData: NodeDisplayData, targetData: NodeDisplayData, data: EdgeDisplayData) {
+  processVisibleItem(
+    i: number,
+    sourceData: NodeDisplayData,
+    targetData: NodeDisplayData,
+    data: EdgeDisplayDataWithLoopInformation,
+  ) {
     const array = this.array;
+
+    const color = floatColor(data.color);
+    const offset = typeof data.offset === "number" ? data.offset : 0;
+    const loopSize = sourceData.size + offset;
+
+    // TODO: angle, offset, edge thickness must not be over loop size
 
     array[i++] = sourceData.x;
     array[i++] = sourceData.y;
+    array[i++] = loopSize;
     array[i++] = data.size;
-    array[i++] = sourceData.size;
-    array[i] = floatColor(data.color);
+    array[i++] = color;
+    array[i++] = ANGLE_1;
+
+    array[i++] = sourceData.x;
+    array[i++] = sourceData.y;
+    array[i++] = loopSize;
+    array[i++] = data.size;
+    array[i++] = color;
+    array[i++] = ANGLE_2;
+
+    array[i++] = sourceData.x;
+    array[i++] = sourceData.y;
+    array[i++] = loopSize;
+    array[i++] = data.size;
+    array[i++] = color;
+    array[i++] = ANGLE_3;
   }
 
   draw(params: RenderParams): void {
     const gl = this.gl;
 
-    const { u_sizeRatio, u_pixelRatio, u_matrix, u_dimensions } = this.uniformLocations;
+    const { u_sizeRatio, u_correctionRatio, u_matrix, u_dimensions } = this.uniformLocations;
 
     gl.uniform1f(u_sizeRatio, params.sizeRatio);
-    gl.uniform1f(u_pixelRatio, params.pixelRatio);
+    gl.uniform1f(u_correctionRatio, params.correctionRatio);
     gl.uniformMatrix3fv(u_matrix, false, params.matrix);
     gl.uniform2f(u_dimensions, params.width * params.pixelRatio, params.height * params.pixelRatio);
 
-    gl.drawArrays(gl.POINTS, 0, this.verticesCount);
+    gl.drawArrays(gl.TRIANGLES, 0, this.verticesCount);
   }
 }
