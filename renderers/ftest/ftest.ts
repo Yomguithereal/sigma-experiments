1075e120c971016718b664148242d0e93f4d7884
Yomguithereal <guillaumeplique@gmail.com>
2022-11-17 15:24:54 +0100

    Spotting several pixel ratio errors

--- renderers/ftest/ftest.ts
+++ renderers/ftest/ftest.ts
@@ -16,7 +16,7 @@ randomLayout.assign(clusteredGraph);
 forceAtlas2.assign(clusteredGraph, { iterations: 100, settings: forceAtlas2.inferSettings(clusteredGraph) });
 
 const dummyGraph = new MultiDirectedGraph();
-dummyGraph.addNode(0, { label: "0", x: 0, y: 1, size: 10 });
+dummyGraph.addNode(0, { label: "0", x: 0, y: 1, size: 10, color: "rgba(255, 0, 0, 0.1)" });
 dummyGraph.addNode(1, { label: "1", x: 2, y: 1, size: 10 });
 dummyGraph.addNode(2, { label: "2", x: 0, y: 0, size: 10 });
 dummyGraph.addNode(3, { label: "3", x: 2, y: 0, size: 10 });
@@ -25,7 +25,8 @@ dummyGraph.mergeEdge(0, 0, { color: "black", type: "loop" });
 // dummyGraph.mergeEdge(0, 0, { color: "black", type: "loop", offset: 10 });
 // dummyGraph.mergeEdge(0, 0, { color: "black", type: "loop", offset: 15 });
 dummyGraph.mergeEdge(0, 1, { color: "blue" });
-dummyGraph.mergeEdge(1, 0, { color: "blue" });
+dummyGraph.mergeEdge(0, 1, { color: "blue", type: "line", size: 20 });
+dummyGraph.mergeEdge(1, 0, { color: "blue", size: 20 });
 dummyGraph.mergeEdge(1, 2, { color: "blue" });
 dummyGraph.mergeEdge(2, 3, { color: "blue" });
 dummyGraph.mergeEdge(3, 0, { color: "blue" });
@@ -41,7 +42,7 @@ shownGraph.updateEachNodeAttributes((node, attr) => {
     ...attr,
     size,
     haloSize: size * 7,
-    color: "red",
+    color: attr.color || "red",
     haloColor: Math.random() > 0.5 ? "blue" : "green",
     insideColor: "yellow",
     dotColor: "black",
--- renderers/src/edge/edge.loop.ts
+++ renderers/src/edge/edge.loop.ts
@@ -15,6 +15,7 @@ attribute float a_angle;
 attribute vec4 a_color;
 
 uniform mat3 u_matrix;
+uniform float u_pixelRatio;
 uniform float u_sizeRatio;
 uniform float u_correctionRatio;
 uniform vec2 u_dimensions;
@@ -40,14 +41,16 @@ vec2 viewportToClipspace(vec2 pos, vec2 dimensions) {
 }
 
 const float bias = 255.0 / 254.0;
-const float marginRatio = 1.05;
 const float theta = 0.78;
-const float minBorderRatio = 0.05;
+const float minThickness = 0.5;
 
 void main() {
   vec2 viewportPosition = clipspaceToViewport(a_position, u_dimensions);
 
-  float viewportRadius = a_size * u_sizeRatio / a_thickness * a_thickness;
+  float viewportRadius = (
+    a_size * u_pixelRatio * u_sizeRatio / 2.0 +
+    a_thickness * u_pixelRatio * u_sizeRatio
+  );
 
   viewportPosition.x += viewportRadius * cos(theta);
   viewportPosition.y += viewportRadius * sin(theta);
@@ -56,7 +59,7 @@ void main() {
 
   float size = a_size * u_correctionRatio / u_sizeRatio * 4.0;
   vec2 diffVector = size * vec2(cos(a_angle), sin(a_angle));
-  position += diffVector  * marginRatio;
+  position += diffVector;
 
   gl_Position = vec4(
     (u_matrix * vec3(position, 1)).xy,
@@ -65,9 +68,9 @@ void main() {
   );
 
   v_border = u_correctionRatio;
-  v_borderRatio = 1.0 - 0.2;
+  v_borderRatio = 1.0 - max(minThickness, a_thickness) / a_size;
   v_diffVector = diffVector;
-  v_radius = size / 2.0 / marginRatio;
+  v_radius = size / 2.0;
 
   v_color = a_color;
   v_color.a *= bias;
@@ -117,7 +120,7 @@ const ANGLE_1 = 0;
 const ANGLE_2 = (2 * Math.PI) / 3;
 const ANGLE_3 = (4 * Math.PI) / 3;
 
-const UNIFORMS = ["u_sizeRatio", "u_correctionRatio", "u_matrix", "u_dimensions"] as const;
+const UNIFORMS = ["u_sizeRatio", "u_correctionRatio", "u_matrix", "u_dimensions", "u_pixelRatio"] as const;
 
 const { FLOAT, UNSIGNED_BYTE } = WebGLRenderingContext;
 
@@ -178,9 +181,10 @@ export default class EdgeLoopProgram extends EdgeProgram<typeof UNIFORMS[number]
   draw(params: RenderParams): void {
     const gl = this.gl;
 
-    const { u_sizeRatio, u_correctionRatio, u_matrix, u_dimensions } = this.uniformLocations;
+    const { u_sizeRatio, u_correctionRatio, u_matrix, u_dimensions, u_pixelRatio } = this.uniformLocations;
 
     gl.uniform1f(u_sizeRatio, params.sizeRatio);
+    gl.uniform1f(u_pixelRatio, params.pixelRatio);
     gl.uniform1f(u_correctionRatio, params.correctionRatio);
     gl.uniformMatrix3fv(u_matrix, false, params.matrix);
     gl.uniform2f(u_dimensions, params.width * params.pixelRatio, params.height * params.pixelRatio);
